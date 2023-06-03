90c17d31b8cbf9a1608b3dff03dd751b45846212
Aaron Gawronsky <agawronsky@gmail.com>
2023-06-03 07:26:04 -0400

    small updates

--- authors/admin.py
+++ authors/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Author
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'dob']
+    list_display = ('name', 'dob')
 
 admin.site.register(Author)
--- books/admin.py
+++ books/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
+    list_display = ('title', 'author')
 
 admin.site.register(Book)
